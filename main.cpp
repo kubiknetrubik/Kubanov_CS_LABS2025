3c1264844663a3fe1e358d542561a3fc329b6d57
kubik <serkub06@yandex.ru>
2025-03-03 23:26:59 +0300

    working bun not good

--- main.cpp
+++ main.cpp
@@ -1,46 +1,22 @@
-#include "fraction.hpp"
+#include "MyStack/MyStack.hpp"
 
 int main(int, char**) {
     try {
-        FractionZone::Fraction f1("2/9");
-        std::cout << f1 << std::endl;
-        std::cout << "Введите дробь: \n";
-        FractionZone::Fraction z;
-        std::cin >> z;
-        std::cout << "z=" << z << std::endl;
-        FractionZone::Fraction fr1(10, 14);
-        FractionZone::Fraction fr2 = 1.25;
-        std::cout << "fr2=" << fr2 << std::endl;
-        std::cout << "fr1=" << fr1 << std::endl;
-        FractionZone::Fraction fr = "-1 4/8";
-        std::cout << "fr=" << fr << std::endl;
-        FractionZone::Fraction x(z), y;
-        std::cout << "x=" << x << std::endl;
-        double dbl = -1.25;
-        FractionZone::Fraction f = dbl;
-        std::cout << "f=" << f << " " << x << " " << z << std::endl;
-        y = x + z;
-        std::cout << "y=" << y << std::endl;
-        y += x;
-        f += dbl / 2;
-        std::cout << "f=" << f << " " << x << " " << dbl << std::endl;
-        y = x + dbl;
-        std::cout << "y=" << y << std::endl;
-        y = dbl + y;
-        std::cout << "y=" << y << std::endl;
-        y += dbl;
-        std::cout << "y=" << y << std::endl;
-        int i = 5;
-        y += i;
-        std::cout << "y=" << y << std::endl;
-        y = i + x;
-        std::cout << "y=" << y << std::endl;
-        y = x + i;
-        std::cout << "y=" << y << std::endl;
-        y += dbl + i + x;
-        std::cout << "y=" << y << std::endl;
-    } catch (...) {
-        std::cout<<"ERROR\n";
+        int N{};
+        std::cout<<"Введите целое число N:"<<std::endl;
+        std::cin>>N;
+        if (N <= 0) {
+            throw std::runtime_error("Число N не валидно.");
+        }
+        MyStack<int> stack;
+        Multipliers(N,stack);
+        MyStack<int> stack2=stack;
+        Print(stack,N,false);
+        Print(stack2,N,true);
+
+
+    } catch (std::runtime_error& e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
     }
 
     return 0;
